3b8dbf39229e911007a1363906b73e81db2b2757
guill94 <86225123+guill94@users.noreply.github.com>
2022-02-28 15:49:44 +0100

    anim

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,11 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import img1 from '../public/images/img1.jpg';
 import Nav from '../components/nav'
-import { useEffect } from 'react';
 import Card from '../components/card';
 
+
+
 export default function Home() {
-  useEffect(() => {
-  const titre = document.querySelector('.bgIndex h1');
-  const info = document.querySelectorAll('.bgIndex p');
-  const bas = document.querySelector('footer');
-
-
-  window.addEventListener('load', () => {
-
-      const TL = gsap.timeline({paused: true});
-
-      TL
-      .staggerFrom(titre, 2.5, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
-      .staggerFrom(info, 2.5, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
-      .from(bas, 2, {transform: "scale(0)", ease: "power2.out"},0.3, '-=2')
-
-      TL.play();
-  })
-  }, [])
-  
-
 
   return (
     <div className={styles.container}>
@@ -40,8 +21,6 @@ export default function Home() {
       <div className="bgIndex">
           
             <h1><span>Bien</span><span>venue</span></h1>
-            <p className="infoIndex">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cntore! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi ullam labore facere</p>
-            <p className="infoIndex">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cntore! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi ullam labore facere</p>
             <p className="infoIndex">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cntore! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi ullam labore facere</p>
         </div>
 
