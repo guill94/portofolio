379da81bfb3cdb8bb01bc54edfc641156d9ec1a4
guill94 <86225123+guill94@users.noreply.github.com>
2022-02-28 16:41:03 +0100

    animations framer motion

--- components/card.jsx
+++ components/card.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const card = (props) => {
   return (
-        <div className="card">
+        <motion.div className="card" whileHover={{scale: 1.1, zIndex: 1}}>
             <Image src={props.img} className="card-img-top" alt={props.alt}/>
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.text}</p>
-                <Link href={props.link}><a className="btn btn-primary">{props.button}</a></Link>
+                <Link href={props.link}>
+                  <motion.a className="btn btn-primary"
+                    whileHover={{ scale: 1.2}}
+                  >
+                    {props.button}
+                  </motion.a>
+                </Link>
             </div>
-        </div>
+        </motion.div>
   )
 }
 
