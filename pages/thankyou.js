b0f0781e86a7a8eb93db9ea2b818c24c04cc86e5
guill94 <86225123+guill94@users.noreply.github.com>
2022-03-02 11:59:46 +0100

    projects

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
+import Link from 'next/link';
 
 
 export async function getStaticProps({ locale }) {
