8d165757c5a7610921e06a0a11b6c392e8f457c8
guill94 <86225123+guill94@users.noreply.github.com>
2022-03-09 12:22:40 +0100

    edit footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
       
       <div className="text-center p-3" >
         © 2021 - {date.getFullYear()}
-        <p className="text-white ms-2">Made by Guillaume BURGNIES with Next J</p>
+        <p className="text-white ms-2">Made by Guillaume BURGNIES with Next JS</p>
       </div>
       
     </footer>
